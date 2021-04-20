06dec2774137bcd6cc1b8782c713c7c17390edfa
Mohamed Ramdane Debiane <mohamed.debiane@ens.uvsq.fr>
2021-04-20 12:13:19 +0200

    Liaison back et front

--- front/src/app/mdl-shared/main-search/main-search.component.ts
+++ front/src/app/mdl-shared/main-search/main-search.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {SharedService} from '../shared.service';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 
 @Component({
   selector: 'app-main-search',
@@ -8,7 +9,7 @@ import {SharedService} from '../shared.service';
 })
 export class MainSearchComponent implements OnInit {
 
-  constructor(private sharedS: SharedService) {}
+  constructor(private sharedS: SharedService, private http: HttpClient) {}
   lgScreen = false;
   states = this.sharedS.states;
 
@@ -22,8 +23,25 @@ export class MainSearchComponent implements OnInit {
   }
 
   doSearch(){
-    let url = ""
-    console.log(this.mission, this.prix, this.code)
-    this.http.
-  }
+    console.log(this.mission, this.prix, this.code);
+    let url = "http://localhost:3000/search?";
+    const params = new HttpParams()
+        .set('metier', this.mission)
+        .set('prix', this.prix.toString())
+        .set('code', this.code.slice(0,2));
+    let headers = new HttpHeaders();
+    headers=headers.append('content-type','application/json');
+    headers=headers.append('Access-Control-Allow-Origin', '*');
+    headers=headers.append('content-type','application/x-www-form-urlencoded');
+    //@ts-ignore
+    this.http.post(url, {},{'header': headers, 'params':params}).subscribe(
+        (res) => {
+          console.log(res);
+        },
+        (err) => {
+          console.error(err);
+        }
+    )
+  };
+
 }
