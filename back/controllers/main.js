f1e8012fc352865cc5498bfa6f0036166a6328bb
Amir <amirhammad27@gmail.com>
2021-05-04 18:30:14 +0200

    modification search annonce

--- back/controllers/main.js
+++ back/controllers/main.js
@@ -4,31 +4,36 @@ module.exports = {
     test: function (req, res, next) {
         res.status(200).json({API :"works"});
     },
-
+    
     //Recherche par mot clé des annonces
-    search:  function(req,res, next) {
+    search: function(req,res, next) {
         let resultat = [];
         let uniqueres = [];
         if(req.body.metier){
             var regex = new RegExp(req.body.metier,'i');
         }
         let annonces = mongoose.connection.collection("annonces");
-
-        annonces.find( { $or:[{code: req.body.code}, {tarifmin: {$lt: req.body.prix} }, {tag: regex}]}
+        console.log(req.body)
+        annonces.find( {tag: req.body.tag}
             , async function(err, curr){
                 if(err) console.error(err);
                 while (await curr.hasNext()){
                     resultat.push(await curr.next());
                 }
-                //Suppression des doublons
+                //console.log(req);
+
                 resultat.forEach((ann) => {
-                    if(!uniqueres.includes(ann)){
+                    if(req.body.code === ' '){ //  && req.body.tarifmax === null
+                        uniqueres.push(ann);
+                    }
+                    if(ann.code === req.body.code && ann.tarifmax <= req.body.tarifmax) { //
+                        console.log("Filtrage par code")
                         uniqueres.push(ann);
                     }
                 })
-                //Code 200 = OK et retourne les résultats
+
+                console.log(uniqueres);
                 res.status(200).json(uniqueres);
             });
-
     }
-};
+}
