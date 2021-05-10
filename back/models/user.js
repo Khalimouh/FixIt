9de1a0ab6439a8d9cee2c7ed4ba163388730116c
mohamed ramdane debiane <mohamed.debiane@ens.uvsq.fr>
2021-05-10 14:04:52 +0200

    Chiffrement Signature des champs sensibles

--- back/models/user.js
+++ back/models/user.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose")
 const bcrypt = require("bcrypt")
+const encrypt = require('mongoose-encryption');
 const { Schema } = mongoose;
+
 
 //Extension de structure de
 const Clientschema = new Schema({
@@ -24,11 +26,20 @@ const Clientschema = new Schema({
     }]
 });
 
+//TODO: Déplacer les clé dans le .env et trouver un moyen d'implémenter un key vault local from scratch
+let encKey = process.AESK
+let signKey = process.HMACK
+
+//Chiffrement et signature des données sensibles des champs sensibles
+//TODO: Ajouter au fûr et a mesure les cfhamps a chiffrer etou signer
+Clientschema.plugin(encrypt, { encryptionKey: encKey, signingKey: signKey , encryptedFields: ['adresse','tel','mail','identité']})
+
 //Création d'un middleware de chiffrement à éxécuter avant l'insertion en base
 Clientschema.pre("save",async function(next){
     const SALT_WORK_FACTOR = 10;
     const salt  = await bcrypt.genSalt(SALT_WORK_FACTOR);
     this.password =  await bcrypt.hash(this.password, salt);
+    console.log(this)
     next();
 });
 
